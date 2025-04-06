dad6e773a51e8176dd45113893f753103775886e
imGazza <gazzardi.luca@gmail.com>
2025-04-06 18:00:53 +0200

    switched to absolute path for reading configs

--- config_reader.py
+++ config_reader.py
@@ -1,7 +1,9 @@
 import yaml
+from pathlib import Path
 
 def load_config():
-    with open('script_config.yaml', 'r') as file:
+    config_path = Path(__file__).parent / 'script_config.yaml'
+    with open(config_path, 'r') as file:
         return yaml.safe_load(file)
 
 def get_config():
